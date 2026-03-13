95bf43b6f712debafc648ae8e80a815ad678b5c4
something <m.noordhoff@me.com>
2026-03-13 16:48:35 +0300

    Use Shopify embedded app provider for admin shell

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,10 +1,22 @@
-import { Link, Outlet } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import { NavMenu } from "@shopify/app-bridge-react";
-import { AppProvider } from "@shopify/polaris";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
+import { authenticate } from "../shopify.server";
+
+export async function loader({ request }) {
+  await authenticate.admin(request);
+
+  return json({
+    apiKey: process.env.SHOPIFY_API_KEY || "",
+  });
+}
 
 export default function App() {
+  const { apiKey } = useLoaderData();
+
   return (
-    <AppProvider i18n={{}}>
+    <AppProvider apiKey={apiKey} isEmbeddedApp>
       <NavMenu>
         <Link to="/app" rel="home">Tournaments</Link>
         <Link to="/app/tournament/new">New Tournament</Link>
